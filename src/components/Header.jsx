a3f6a9522f1824b7df3bcdc373b27939bd9f6f37
MATHEUS <matts14smkd@gmail.com>
2024-06-27 18:05:41 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import SearchForm from "./SearchForm";
 function Header() {
   return (
     <header className="bg-stone-50 mb-4 h-auto py-3 px-4 rounded-xl flex flex-wrap gap-3 sm:grid sm:grid-cols-6 sm:gap-y-0">
-      <SearchForm className="col-span-3 mx-auto justify-self sm:grid-cols-5 order-1 sm:order-none" />
+      <SearchForm className="col-span-2 mx-auto sm:mx-0 sm:grid-cols-5 order-1 sm:order-none" />
       <Dropdown />
     </header>
   );
